d25018d0e6f949a8375495ae41caf8b450ccfb4f
patrick-zvenyka <patrickzvenyika1999@gmail.com>
2025-01-09 15:52:47 +0200

    landin-page

--- lms/student/urls.py
+++ lms/student/urls.py
@@ -4,4 +4,5 @@ from django.contrib import admin
 
 urlpatterns = [
     path('', Login, name='stud-login'),
+    path('register', Register, name='stud-register'),
 ]
--- lms/student/views.py
+++ lms/student/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def Login(request):
     return render(request, 'Student/sign-in.html')
+
+def Register(request):
+    return render(request, 'Student/sign-up.html')
