c91bb0dd9eb383db9495df8c020f59ccc6783435
patrick-zvenyka <patrickzvenyika1999@gmail.com>
2025-01-10 11:28:27 +0200

    librarian-side-comp-1

--- lms/library/urls.py
+++ lms/library/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('', Login, name='lib-login'),
+    path('librarian/logout/', Logout, name='logout'),
     path('home', Dashboard, name='lib-dashboard'),
+    path('books', Books, name='lib-book-list'),
+    path('billing', Billing, name='lib-billing'),
 ]
--- lms/library/views.py
+++ lms/library/views.py
@@ -1,8 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render,  redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def Login(request):
     return render(request, 'Librarian/sign-in.html')
 
+def Logout(request):
+    logout(request)
+    return redirect('lib-login')  
+
 def Dashboard(request):
-    return render(request, 'Librarian/dashboard.html')
+
+    context = {
+        'title':'Dashboard'
+    }
+    return render(request, 'Librarian/dashboard.html', context)
+
+def Books(request):
+
+    context = {
+        'title':'Books'
+    }
+    return render(request, 'Librarian/books-list.html', context)
+
+
+def Billing(request):
+    
+    context = {
+        'title' : 'Billing'
+    }
+    return render(request, 'Librarian/billing.html', context)
+
+def Profile(request):
+
+    context = {
+        'title' : 'Profile'
+    }
+    return render(request, 'profile.html', context)
