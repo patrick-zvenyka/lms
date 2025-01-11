6360bdb4a57b36e625a36be838667438a3cf627a
patrick-zvenyka <patrickzvenyika1999@gmail.com>
2025-01-11 10:54:12 +0200

    change-of-icons

--- lms/library/views.py
+++ lms/library/views.py
@@ -1,15 +1,32 @@
 from django.shortcuts import render,  redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
-
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
 # Create your views here.
 def Login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")  # Get username from the input name
+        password = request.POST.get("password")  # Get password from the input name
+        
+        user = authenticate(request, username=username, password=password)  # Authenticate user
+        if user is not None:
+            if user.groups.filter(name='Librarians').exists():  # Check if the user is in the 'Librarians' group
+                login(request, user)
+                return redirect('lib-dashboard')  # Redirect to dashboard
+            else:
+                messages.error(request, "You do not have the required permissions to access this system.")
+        else:
+            messages.error(request, "Invalid username or password.")
+
     return render(request, 'Librarian/sign-in.html')
 
 def Logout(request):
     logout(request)
     return redirect('lib-login')  
 
+@login_required(login_url='lib-login')
 def Dashboard(request):
 
     context = {
@@ -17,6 +34,7 @@ def Dashboard(request):
     }
     return render(request, 'Librarian/dashboard.html', context)
 
+@login_required(login_url='lib-login')
 def Books(request):
 
     context = {
@@ -24,7 +42,7 @@ def Books(request):
     }
     return render(request, 'Librarian/books-list.html', context)
 
-
+@login_required(login_url='lib-login')
 def Billing(request):
     
     context = {
@@ -32,6 +50,7 @@ def Billing(request):
     }
     return render(request, 'Librarian/billing.html', context)
 
+@login_required(login_url='lib-login')
 def Profile(request):
 
     context = {
